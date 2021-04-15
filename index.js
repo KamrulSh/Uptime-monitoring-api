51b65cdc1e3f769c8b5bc87814706e25776f7976
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2021-04-15 15:20:57 +0600

    request handling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 // dependencies
 const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
 
 // app config
 const app = {};
@@ -20,6 +22,24 @@ app.createServer = () => {
 
 // handle req res
 app.serverReqRes = (req, res) => {
+    // request handle
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimedPath = path.replace(/^\/+|\/+$/g, "");
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+    const decoder = new StringDecoder("utf-8");
+    let realData = "";
+    req.on("data", (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on("end", () => {
+        realData += decoder.end();
+        console.log(realData);
+    });
+    console.log(headersObject);
 
     // response handle
     res.end("Server started");
